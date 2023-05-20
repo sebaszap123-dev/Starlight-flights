996d8a81d29b476d4af32869558af67545ca016c
sebaszap123-dev <freeway.dev.sf@gmail.com>
2023-05-20 14:02:10 -0600

    added fiirebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Google auth
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+
+export { app, analytics, auth, provider };
