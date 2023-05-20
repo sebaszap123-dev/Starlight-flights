ac7f72f0d7b996239d93776bfcd435ee7eb66758
sebastian frausto <100221988+sebaszap123-dev@users.noreply.github.com>
2023-05-20 14:02:39 -0600

    Develop (#3)
    * add eslint
    
    * eslint
    
    * added fiirebase auth

--- src/provider/api.js
+++ src/provider/api.js
@@ -1,29 +1,9 @@
 import axios from 'axios';
 import { Convert } from '@/models/flights';
 
-
 export const apiStarlight = () => {
     const searchFlights = () => {
-        axios
-            .get('https://test.api.amadeus.com/v2/shopping/flight-offers', {
-                headers: {
-                    Authorization: 'Bearer fAnyQwzmyGNGymXDqpbilYKASE8b'
-                },
-                params: {
-                    originLocationCode: 'SYD',
-                    destinationLocationCode: 'BKK',
-                    departureDate: '2023-05-11',
-                    adults: 1,
-                    nonStop: false,
-                    max: 250
-                }
-            })
-            .then(
-                (response) => {
-                    let flight = Convert.toSearchFlight(JSON.stringify(response.data.data[0]))
-                    console.log(flight.id);
-                }
-            )
+
     }
     return { searchFlights }
 }
